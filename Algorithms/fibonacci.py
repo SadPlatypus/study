81caa937095876021aee0c6094e90983443214fb
panovss <saimonpanov@gmail.com>
2021-07-27 10:11:31 +0300

    Вычисление n-го числа ряда Фибоначчи

---
+++ Algorithms/fibonacci.py
@@ -0,0 +1,10 @@
+def calc_fib(n):
+    fib1 = fib2 = 1
+    i = 0
+    while i < n - 2:
+        fib_sum = fib1 + fib2
+        fib1 = fib2
+        fib2 = fib_sum
+        i += 1
+
+    return fib2
